fca34e2bcaacb83a3abc669fc34a4972479adf7b
alphaAndNumeric <sandeepbansal777e@gmail.com>
2019-05-26 09:33:20 +0530

    Some basic data types in cpp

---
+++ datatypes.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  char grade = 'A';
+  string greet = "Good Morning!";
+  int marks = 87;
+  float avg = 85.8;
+  double gpa = 4.5;
+  bool isMale = true;
+
+  cout << grade << endl;
+  return 0;
+}
